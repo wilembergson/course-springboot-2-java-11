fe51889e4113115dcbc82cb7cf41d34eefbc598a
wilembergson <wilembergson@yahoo.com>
2020-07-24 13:39:57 -0300

    Exception handling - delete

--- src/main/java/com/edicandoweb/course/services/UserService.java
+++ src/main/java/com/edicandoweb/course/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.edicandoweb.course.entities.User;
@@ -30,7 +32,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public User update(Long id, User obj) {
